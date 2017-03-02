19523cbb822c818d8fe19ef2a811c32c17cc542c
Eliot Carlsen, Max Larson and Rob McKenzie <max@>
2017-03-02 13:11:36 -0800

    add cpu player 1 functionality

--- js/scripts.js
+++ js/scripts.js
@@ -56,25 +56,69 @@ function Turn (character, clickCounter1){
   this.clickCounter1 = clickCounter1;
 };
 
+
+
 var player1 = new Turn("X", 0);
 var player2 = new Turn("O", 1);
 
-var clicks = 1;
-Turn.prototype.clickCounter = function(){
-  clicks += 1
-    if  (clicks % 2 === 0) {
-      return player1.character;
-    } else if   (clicks % 2 !== 0){
-          return player2.character;
-      }
-  };
 
+
+newGrid = ["A1","B1","C1","A2","B2","C2","A3","B3","C3"];
+
+
+var computerChoice = function(){
+  var nRandom = Math.floor(Math.random() * (newGrid.length)),
+  mElement = newGrid[nRandom];
+   $("#" + mElement + "").text("O")
+};
+
+
+
+
+var gameTypeToggle = function(gameType){
+if(gameType === 2){
+  var clicks = 1;
+  Turn.prototype.clickCounter = function(){
+    clicks += 1
+      if  (clicks % 2 === 0) {
+        return player1.character;
+      } else if   (clicks % 2 !== 0){
+        return player2.character;
+      }
+    }
+  } else if (gameType === 1) {
+    var clicks = 2;
+    Turn.prototype.clickCounter = function(loc){
+        if  (clicks % 2 === 0) {
+          clicks += 2;
+          $(loc).text("X");
+          computerChoice();
+        }
+      }
+    };
+};
+// }else if(gameType = onePLayer){
+// function ComputerTurn (character){
+//   this.character = character;
+// }
+//   var playerCpu1 = new ComputerTurn("X");
+//   var Cpu = new ComputerTurn("O");
+//
+//   ComputerTurn.prototype.computerTurns = function(square){
+//
+//       if  (b2 === "") {
+//         return win.b2 = Cpu;
+//       }else if   (){
+//             return ;
+//         }
+//     };
+// }
 //frontend
 
 $(document).ready(function(){
   $("#a1").click(function(event){
     event.preventDefault();
-    var result = player1.clickCounter();
+    var result = player1.clickCounter("#A1");
     $("#A1").text(result);
     win.a1 = result;
     win.winner();
@@ -82,7 +126,7 @@ $(document).ready(function(){
   });
   $("#b1").click(function(event){
     event.preventDefault();
-    var result = player1.clickCounter();
+    var result = player1.clickCounter("#B1");
     $("#B1").text(result);
     win.b1 = result;
     win.winner();
@@ -90,7 +134,7 @@ $(document).ready(function(){
   });
   $("#c1").click(function(event){
     event.preventDefault();
-    var result = player1.clickCounter();
+    var result = player1.clickCounter("#C1");
     $("#C1").text(result);
     win.c1 = result;
     win.winner();
@@ -98,7 +142,7 @@ $(document).ready(function(){
   });
   $("#a2").click(function(event){
     event.preventDefault();
-    var result = player1.clickCounter();
+    var result = player1.clickCounter("#A2");
     $("#A2").text(result);
     win.a2 = result;
     win.winner();
@@ -106,7 +150,7 @@ $(document).ready(function(){
   });
   $("#b2").click(function(event){
     event.preventDefault();
-    var result = player1.clickCounter();
+    var result = player1.clickCounter("#B2");
     $("#B2").text(result);
     win.b2 = result;
     win.winner();
@@ -114,7 +158,7 @@ $(document).ready(function(){
   });
   $("#c2").click(function(event){
     event.preventDefault();
-    var result = player1.clickCounter();
+    var result = player1.clickCounter("#C2");
     $("#C2").text(result);
     win.c2 = result;
     win.winner();
@@ -122,7 +166,7 @@ $(document).ready(function(){
   });
   $("#a3").click(function(event){
     event.preventDefault();
-    var result = player1.clickCounter();
+    var result = player1.clickCounter("#A3");
     $("#A3").text(result);
     win.a3 = result;
     win.winner();
@@ -130,7 +174,7 @@ $(document).ready(function(){
   });
   $("#b3").click(function(event){
     event.preventDefault();
-    var result = player1.clickCounter();
+    var result = player1.clickCounter("#B3");
     $("#B3").text(result);
     win.b3 = result;
     win.winner();
@@ -138,10 +182,16 @@ $(document).ready(function(){
   });
   $("#c3").click(function(event){
     event.preventDefault();
-    var result = player1.clickCounter();
+    var result = player1.clickCounter("#C3");
     $("#C3").text(result);
     win.c3 = result;
     win.winner();
     $("#c3").unbind("click");
   });
+  //one player Game
+  $(".form").click(function(event){
+    event.preventDefault();
+    var gameType = parseInt($("input:radio[name=playerSelect]:checked").val());
+    gameTypeToggle(gameType);
+ });
 });
